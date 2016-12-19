992862bce47d0bc5bbf90e14892337c9f8871f43
J Irving <j@lollyshouse.ca>
2016-12-19 13:49:54 -0500

    Add a rake task to update monitors

--- lib/appcanary/railtie.rb
+++ lib/appcanary/railtie.rb
@@ -6,6 +6,7 @@ module Appcanary
       spec = Gem::Specification.find_by_name("appcanary")
       gem_root = spec.gem_dir
       load "#{gem_root}/lib/appcanary/tasks/appcanary/check.rake"
+      load "#{gem_root}/lib/appcanary/tasks/appcanary/monitor.rake"
     end
   end
 end
